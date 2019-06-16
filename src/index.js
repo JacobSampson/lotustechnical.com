36a466c4f10e8ffa11dcf3208655b459130fee68
Jacob Sampson <jacob.sampson@huskers.unl.edu>
2019-06-16 12:21:47 -0500

    Updated navigation to other pages

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-    $("main").load("pages/landing.html");
+    setMain("pages/landing.html");
+
+    $("#logo-technical").click(() => setMain("pages/landing.html"));
+
+    // Main navigation links
+    $("#link-clients").click(() => setMain("pages/clients.html"));
+    $("#link-job-seekers").click(() => setMain("pages/job-seekers.html"));
+    $("#link-employment-forms").click(() => setMain("pages/employment-forms.html"));
+    $("#link-openings").click(() => setMain("pages/openings.html"));
+    $("#link-resume").click(() => setMain("pages/resume.html"));
 });
+
+function setMain(page) {
+    $("main").load(page);
+}
