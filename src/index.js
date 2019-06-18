c330786e74dfaef1ab78e077461537053b549473
Jacob Sampson <jacob.sampson@huskers.unl.edu>
2019-06-17 19:00:30 -0500

    Continued updated style on main

--- src/index.js
+++ src/index.js
@@ -3,14 +3,44 @@ $(document).ready(function() {
     setMain("pages/landing.html");
     
     // Main navigation links
-    $("#logo-technical").click(() => setMain("pages/landing.html"));
-    $("#link-landing").click(() => setMain("pages/landing.html"));
-    $("#link-clients").click(() => setMain("pages/clients.html"));
-    $("#link-job-seekers").click(() => setMain("pages/job-seekers.html"));
-    $("#link-employment-forms").click(() => setMain("pages/employment-forms.html"));
-    $("#link-openings").click(() => setMain("pages/openings.html"));
+    $("#logo-technical").click(function() {
+        setMain("pages/landing.html");
+        removeActive();
+        $(this).addClass("active");
+    });
+    $("#link-landing").click(function() {
+        setMain("pages/landing.html");
+        removeActive();
+        $(this).addClass("active");
+    });
+    $("#link-clients").click(function() {
+        setMain("pages/clients.html");;
+        removeActive();
+        $(this).addClass("active");
+    });
+    $("#link-job-seekers").click(function() {
+        setMain("pages/job-seekers.html");
+        removeActive();
+        removeActive();
+        $(this).addClass("active");
+    });
+    $("#link-employment-forms").click(function() {
+        setMain("pages/employment-forms.html");
+        removeActive();
+        removeActive();
+        $(this).addClass("active");
+    });
+    $("#link-openings").click(function() {
+        setMain("pages/openings.html");
+        removeActive();
+        $(this).addClass("active");
+    });
 });
 
 function setMain(page) {
     $("main").load(page);
+}
+
+function removeActive() {
+    $(".inner-link").removeClass("active");
 }
