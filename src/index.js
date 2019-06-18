9ac82745e959daaf3d95d1986556645c54c1d9cc
Jacob Sampson <jacob.sampson@huskers.unl.edu>
2019-06-18 18:51:42 -0500

    Updated style on clients page?

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     // Set landing as the default home page
-    $("main").load("pages/landing.html");
+    $("main").load("pages/job-seekers.html");
 
     // Setup nav button styles
     $("nav").load("pages/nav.html", function() {
