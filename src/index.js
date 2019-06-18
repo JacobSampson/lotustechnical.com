7fdd705421781c30ee3f34c23e41f0ba21ea084f
Jacob Sampson <jacob.sampson@huskers.unl.edu>
2019-06-17 22:17:50 -0500

    Updated arrow icon

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
 $(document).ready(function() {
     // Set landing as the default home page
-    $("main").load("pages/landing.html");
+    $("main").load("pages/job-seekers.html");
 
     $("nav").load("pages/nav.html", function() {
-        $("nav").on('click', 'li', function() {
-            $('.active').removeClass('active');
-            $(this).addClass('active');
+        // Add and remove active state
+        $("nav").on("click", "li", function() {
+            $(".active").removeClass("active");
+            $(this).addClass("active");
         });
 
         // Main navigation links
         $("#logo-technical").click(function() {
             $("main").load("pages/landing.html");
-            $(this).addClass("active");
         });
         $("#link-landing").click(function() {
             $("main").load("pages/landing.html");
-            $(this).addClass("active");
         });
         $("#link-clients").click(function() {
             $("main").load("pages/clients.html");;
-            $(this).addClass("active");
         });
         $("#link-job-seekers").click(function() {
             $("main").load("pages/job-seekers.html", function() {
@@ -27,20 +25,19 @@ $(document).ready(function() {
                 $(".info-bubble").click(function() {
                     if ($(this).hasClass("active")) {
                         $(this).removeClass("active");
+                        $(this).next().removeClass("active");
                     } else {
                         $(this).addClass("active");
+                        $(this).next().addClass("active");
                     }
                 });
             });
-            $(this).addClass("active");
         });
         $("#link-employment-forms").click(function() {
             $("main").load("pages/employment-forms.html");
-            $(this).addClass("active");
         });
         $("#link-openings").click(function() {
             $("main").load("pages/openings.html");
-            $(this).addClass("active");
         });
     });
 });
